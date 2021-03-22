46adfe6a1474770c91ac6f01536661323da88006
KaiTang2 <1347102146@qq.com>
2021-03-22 22:24:47 +0800

    Question 191

---
+++ LeetCodeProject/Question191/Question191.cpp
@@ -0,0 +1,21 @@
+﻿#include"Question191.h"
+int hammingWeight(uint32_t n) {
+    int ret = 0;
+    while (n != 0)
+    {
+        n = n & (n - 1);
+        ret++;
+    }
+    return ret;
+}
+int main()
+{
+    /*
+    编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+    */
+    uint32_t k = 6;
+    cout << hammingWeight(k) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
