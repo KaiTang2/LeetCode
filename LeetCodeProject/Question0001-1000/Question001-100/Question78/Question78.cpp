937ae8f3ed7959f5d8edacb7f81a70ba14d1a862
KaiTang2 <1347102146@qq.com>
2021-03-31 22:52:25 +0800

    Qusetion 78 90 同一系列问题

---
+++ LeetCodeProject/Question0001-1000/Question001-100/Question78/Question78.cpp
@@ -0,0 +1,50 @@
+﻿#include"Question78.h"
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    int n = nums.size();
+    for (int mask = 0; mask < (1 << n); mask++)
+    {
+        vector<int> t;
+        for (int i = 0; i < n; i++)
+        {
+            if (mask & (1 << i))
+            {
+                t.push_back(nums[i]);
+            }
+        }
+        result.push_back(t);
+    }
+    return result;
+}
+int main()
+{
+    /*
+    给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+示例 1：
+输入：nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+示例 2：
+输入：nums = [0]
+输出：[[],[0]]
+提示：
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+nums 中的所有元素 互不相同
+
+
+    */
+    vector<int> vec = { 1,2,3 };
+    vector<vector<int>> res = subsets(vec);
+    for each (auto var1 in res)
+    {
+        for each (auto var2 in var1)
+        {
+            cout << var2 << " ";
+        }
+        cout << endl;
+    }
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question0001-1000/Question001-100/Question90/Question90.cpp
@@ -0,0 +1,58 @@
+﻿#include"Question90.h"
+#include<algorithm>
+vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    vector<vector<int>> result;
+    for (int mask = 0; mask < (1 << n); mask++)
+    {
+        bool input = true;
+        vector<int> t;
+        for (int i = 0; i < n; i++)
+        {
+            if (mask & (1 << i))
+            {
+                if (i > 0 && (mask >> (i - 1) & 1) == 0 && nums[i] == nums[i - 1]) {
+                    input = false;
+                    break;
+                }
+                t.push_back(nums[i]);
+            }
+        }
+        if (input)
+        {
+            result.push_back(t);
+        }
+    }
+    return result;
+}
+int main()
+{
+    /*
+    给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+示例 1：
+输入：nums = [1,2,2]
+输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+示例 2：
+输入：nums = [0]
+输出：[[],[0]]
+
+提示：
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+    */
+    vector<int> vec = { 1,2,2 };
+    vector<vector<int>> res = subsetsWithDup(vec);
+    for each (auto var1 in res)
+    {
+        for each (auto var2 in var1)
+        {
+            cout << var2 << " ";
+        }
+        cout << endl;
+    }
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
