6afb029006bc2fd4ee2cd90aa8b722e8fc09ff93
KaiTang2 <1347102146@qq.com>
2021-04-02 22:47:24 +0800

    InterviewQuestion17.21 一看题目难度：困看，我靠！在看题目内容，emmm，蓄水池问题，那没事了。

---
+++ LeetCodeProject/InterviewQuestion/1721/1721.cpp
@@ -0,0 +1,44 @@
+﻿#include"1721.h"
+#include<numeric>
+#include<algorithm>
+int trap(vector<int>& height) {
+    int sum = accumulate(height.begin(), height.end(), 0);
+    int all = 0;
+    int h = 1;
+    int size = height.size();
+    int left = 0;
+    int right = size - 1;
+    while (left <= right)
+    {
+        while (left <= right && height[left] < h)
+        {
+            left++;
+        }
+        while (left <= right && height[right] < h)
+        {
+            right--;
+        }
+        all += right - left + 1;
+        h++;
+    }
+    return all - sum;
+}
+int main()
+{
+    /*
+    给定一个直方图(也称柱状图)，假设有人从上面源源不断地倒水，最后直方图能存多少水量?直方图的宽度为 1。
+上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的直方图，在这种情况下，可以接 6 个单位的水（蓝色部分表示水）。 感谢 Marcos 贡献此图。
+示例:
+输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+输出: 6
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/volume-of-histogram-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+    */
+    vector<int> vec = { 0,1,0,2,1,0,1,3,2,1,3,1 };
+    cout << trap(vec) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
