72180bd4ec0262deaf752a70b3aee4cc7eb1ec65
KaiTang2 <1347102146@qq.com>
2021-04-03 20:01:11 +0800

    Question 136 InterviewQuestion1610 时间和内存都大

---
+++ LeetCodeProject/InterviewQuestion/1610/1610.cpp
@@ -0,0 +1,58 @@
+﻿#include"1610.h"
+#include<map>
+int maxAliveYear(vector<int>& birth, vector<int>& death) {
+    int n = birth.size();
+    vector<int> data(102, 0);
+    for (int i = 0; i < n; i++)
+    {
+        data[birth[i] - 1900] += 1;
+        data[death[i] - 1899] -= 1;
+    }
+    int max = 0, year = 0, sum = 0;
+    for (int i = 0; i <= 100; i++)
+    {
+        sum += data[i];
+        if (max < sum)
+        {
+            max = sum;
+            year = i;
+        }
+    }
+    return year + 1900;
+}
+int maxAliveYear1(vector<int>& birth, vector<int>& death) {
+    int n = birth.size();
+    map<int, int> data;
+    for (int i = 0; i < n; i++)
+    {
+        data[birth[i] - 1900] += 1;
+        data[death[i] - 1899] -= 1;
+    }
+    n = 0;
+    int year = 0, sum = 0;
+    for (auto i = data.begin(); i != data.end(); i++)
+    {
+        sum += i->second;
+        if (n < sum)
+        {
+            n = sum;
+            year = i->first;
+        }
+    }
+    return year + 1900;
+}
+int main()
+{
+    /*
+    
+    [1972,1908,1915,1957,1960,1948,1912,1903,1949,1977,1900,1957,1934,1929,1913,1902,1903,1901]
+[1997,1932,1963,1997,1983,2000,1926,1962,1955,1997,1998,1989,1992,1975,1940,1903,1983,1969]
+    */
+    vector<int> birth = { 1972,1908,1915,1957,1960,1948,1912,1903,1949,1977,1900,1957,1934,1929,1913,1902,1903,1901 };
+    vector<int> death = { 1997,1932,1963,1997,1983,2000,1926,1962,1955,1997,1998,1989,1992,1975,1940,1903,1983,1969 };
+    cout << maxAliveYear(birth, death) << endl;
+    cout << maxAliveYear1(birth, death) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question0001-1000/Question101-200/Question136/Question136.cpp
@@ -0,0 +1,34 @@
+﻿#include"Question136.h"
+#include<map>
+int singleNumber(vector<int>& nums) {
+    int res = 0;
+    for (auto i : nums)
+    {
+        res ^= i;
+    }
+    return res;
+}
+int main()
+{
+    /*
+    给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+说明：
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+示例 1:
+输入: [2,2,1]
+输出: 1
+示例 2:
+输入: [4,1,2,1,2]
+输出: 4
+    */
+    map<int, int> res;
+    res[1] = 3;
+    res[2] += 4;
+    auto it = res.begin();
+    int k = it->second;
+    vector<int> vec = { 4,1,2,1,2 };
+    cout << singleNumber(vec) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
