763f3d87fff40367f63c43b7617c3aa53ebf7592
KaiTang2 <1347102146@qq.com>
2021-04-05 18:21:52 +0800

    Question88 1456

---
+++ LeetCodeProject/Question0001-1000/Question001-100/Question88/Question88.cpp
@@ -0,0 +1,51 @@
+﻿#include"Question88.h"
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int size = nums1.size() - 1;
+    if (m + n < size) { return; }
+    while (m > 0 && n > 0)
+    {
+        if (nums1[m - 1] > nums2[n - 1])
+        {
+            nums1[size] = nums1[m - 1];
+            nums1[m - 1] = 0;
+            m--;
+        }
+        else
+        {
+            nums1[size] = nums2[n-1];
+            n--;
+        }
+        size--;
+    }
+    if (m == 0)
+    {
+        while (size >= 0)
+        {
+            nums1[size] = nums2[n-1];
+            size--;
+            n--;
+        }
+    }
+    for (int i = 0, j = 5; i < 3; i++)
+    {
+
+    }
+}
+int main()
+{
+    /*
+    
+
+    */
+    vector<int> num1 = { 1,2,3,0,0,0 };
+    vector<int> num2 = { 2,5,6 };
+    merge(num1, 3, num2, 3);
+    for (auto i : num1)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question1001-2000/Question1401-1500/Question1456/Question.cpp
@@ -0,0 +1,68 @@
+﻿#include"Question.h"
+bool isVowel(char c)
+{
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+int maxVowels(string s, int k) {
+    int len = s.length();
+    int left = 0;
+    int right = k - 1;
+    int maxLength = 0;
+    int lastLength = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if (isVowel(s[i]))
+        {
+            lastLength++;
+        }
+    }
+    maxLength = lastLength;
+    for (int i = k, j = 0; i < len; i++, j++)
+    {
+        if (isVowel(s[j]))
+        {
+            lastLength -= 1;
+        }
+        if (isVowel(s[i]))
+        {
+            lastLength += 1;
+        }
+        maxLength = max(maxLength, lastLength);
+    }
+    return maxLength;
+}
+int main()
+{
+    /*
+    给你字符串 s 和整数 k 。
+请返回字符串 s 中长度为 k 的单个子字符串中可能包含的最大元音字母数。
+英文中的 元音字母 为（a, e, i, o, u）。
+示例 1：
+输入：s = "abciiidef", k = 3
+输出：3
+解释：子字符串 "iii" 包含 3 个元音字母。
+示例 2：
+输入：s = "aeiou", k = 2
+输出：2
+解释：任意长度为 2 的子字符串都包含 2 个元音字母。
+示例 3：
+输入：s = "leetcode", k = 3
+输出：2
+解释："lee"、"eet" 和 "ode" 都包含 2 个元音字母。
+示例 4：
+输入：s = "rhythms", k = 4
+输出：0
+解释：字符串 s 中不含任何元音字母。
+示例 5：
+输入：s = "tryhard", k = 4
+输出：1
+提示：
+1 <= s.length <= 10^5
+s 由小写英文字母组成
+1 <= k <= s.length
+    */
+    cout << maxVowels("abciiidef", 3) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
