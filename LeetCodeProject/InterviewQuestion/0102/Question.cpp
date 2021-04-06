5e343a89ba1546475746a2d7faf54ffc9ad49b20
KaiTang2 <1347102146@qq.com>
2021-04-06 22:41:29 +0800

    IntervieweQuestion0102

---
+++ LeetCodeProject/InterviewQuestion/0102/Question.cpp
@@ -0,0 +1,31 @@
+﻿#include"Question.h"
+bool CheckPermutation(string s1, string s2) {
+    vector<int> hash(26, 0);
+    for (auto s : s1)
+    {
+        hash[s - 'a']++;
+    }
+    for (auto s : s2)
+    {
+        hash[s - 'a']++;
+    }
+    for (auto i : hash)
+    {
+        if (i != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    /*
+    
+
+    */
+    cout << CheckPermutation("abc", "bca");
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
