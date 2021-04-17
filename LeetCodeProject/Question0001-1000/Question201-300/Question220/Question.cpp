dfaf1ee81bf9111b12a377b4458a5f4a7deb4486
KaiTang2 <1347102146@qq.com>
2021-04-17 20:39:39 +0800

    Question 220 1481

---
+++ LeetCodeProject/Question0001-1000/Question201-300/Question220/Question.cpp
@@ -0,0 +1,61 @@
+﻿/*
+给你一个整数数组 nums 和两个整数 k 和 t 。请你判断是否存在 两个不同下标 i 和 j，使得 abs(nums[i] - nums[j]) <= t ，同时又满足 abs(i - j) <= k 。
+
+如果存在则返回 true，不存在返回 false。
+
+ 
+
+示例 1：
+
+输入：nums = [1,2,3,1], k = 3, t = 0
+输出：true
+示例 2：
+
+输入：nums = [1,0,1,1], k = 1, t = 2
+输出：true
+示例 3：
+
+输入：nums = [1,5,9,1,5,9], k = 2, t = 3
+输出：false
+ 
+
+提示：
+
+0 <= nums.length <= 2 * 104
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 104
+0 <= t <= 231 - 1
+
+
+*/
+#include"Question.h"
+#include<set>
+bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    int len = nums.size();
+    if (len <= 1)
+    {
+        return false;
+    }
+    set<int> mp;
+    for (int i = 0; i < len; i++)
+    {
+        auto it = mp.lower_bound(max(nums[i], INT_MIN + t) - t);
+        if (it != mp.end() && *it <= min(nums[i], INT_MAX - t) + t)
+        {
+            return true;
+        }
+        mp.insert(nums[i]);
+        if (i >= k)
+        {
+            mp.erase(nums[i - k]);
+        }
+    }
+    return false;
+}
+int main()
+{
+
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question1001-2000/Question1401-1500/Question1481/Question.cpp
@@ -0,0 +1,42 @@
+﻿/*
+
+
+*/
+#include"Question.h"
+#include<unordered_map>
+#include<algorithm>
+
+int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+    unordered_map<int, int> mp;
+    for (auto i : arr)
+    {
+        ++mp[i];
+    }
+    vector<pair<int, int>> p(mp.begin(),mp.end());
+    sort(p.begin(), p.end(), [](const pair<int, int>& a, const pair<int, int>& b) { return a.second < b.second; });
+    int res = p.size();
+    for (auto i : p)
+    {
+        if (k <= 0)
+        {
+            break;
+        }
+        if (i.second > k)
+        {
+            ++res;
+        }
+        --res;
+        k -= i.second;
+    }
+    return res;
+
+}
+int main()
+{
+    /*vector<int> nums = { 4,3,1,1,3,3,2 };
+    cout << findLeastNumOfUniqueInts(nums, 3) << endl;*/
+    vector<int> nums = { 5,5,4 };
+    cout << findLeastNumOfUniqueInts(nums, 1) << endl;
+    int pass = getchar();
+    return 0;
+}
