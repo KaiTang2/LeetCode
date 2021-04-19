9364e106980e1d33b980bb67e5639370737618f5
KaiTang2 <1347102146@qq.com>
2021-04-19 22:47:02 +0800

    Question26 27 539 1604

---
+++ LeetCodeProject/Question0001-1000/Question501-600/Question539/Question.cpp
@@ -0,0 +1,49 @@
+﻿/*
+给定一个 24 小时制（小时:分钟 "HH:MM"）的时间列表，找出列表中任意两个时间的最小时间差并以分钟数表示。
+
+ 
+
+示例 1：
+
+输入：timePoints = ["23:59","00:00"]
+输出：1
+示例 2：
+
+输入：timePoints = ["00:00","23:59","00:00"]
+输出：0
+ 
+
+提示：
+
+2 <= timePoints <= 2 * 104
+timePoints[i] 格式为 "HH:MM"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-time-difference
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+#include"Question.h"
+#include<algorithm>
+int ToMin(string& s)
+{
+    return (s[0] - '0') * 600 + (s[1] - '0') * 60 + (s[3] - '0') * 10 + (s[4] - '0');
+}
+int findMinDifference(vector<string>& timePoints) {
+    sort(timePoints.begin(), timePoints.end());
+    int res = INT_MAX;
+    for (int i = 0; i < timePoints.size() - 1; i++)
+    {
+        res = min(res, ToMin(timePoints[i + 1]) - ToMin(timePoints[i]));
+    }
+    res = min(res, 1440 - ToMin(timePoints[timePoints.size() - 1]) + ToMin(timePoints[0]));
+    return res;
+}
+int main()
+{
+    vector<string> timePoints = { "00:00","23:59","00:00" };
+    cout << findMinDifference(timePoints) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question1001-2000/Question1601-1700/Question1604/Question.cpp
@@ -0,0 +1,55 @@
+﻿/*
+
+
+*/
+#include"Question.h"
+#include<unordered_map>
+#include<functional>
+#include<algorithm>
+int ToMin(string& s)
+{
+    return (s[0] - '0') * 600 + (s[1] - '0') * 60 + (s[3] - '0') * 10 + (s[4] - '0');
+}
+vector<string> alertNames(vector<string>& keyName, vector<string>& keyTime) {
+    unordered_map<string, vector<int>> all;
+    vector<string> res;
+    for (int i = 0; i < keyName.size(); i++)
+    {
+        auto it = all.find(keyName[i]);
+        if (it == all.end())
+        {
+            all.insert(unordered_map<string, vector<int>>::value_type(keyName[i], vector<int>{ToMin(keyTime[i])}));
+        }
+        else
+        {
+            it->second.push_back(ToMin(keyTime[i]));
+        }
+    }
+    for (auto it : all)
+    {
+        if (it.second.size() <= 2)
+        {
+            continue;
+        }
+        sort(it.second.begin(), it.second.end());
+        for (int i = 0; i < it.second.size() - 2; ++i)
+        {
+            if (it.second[i + 2] - it.second[i] <= 60)
+            {
+                res.push_back(it.first);
+                break;
+            }
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+int main()
+{
+    vector<string> name = { "daniel","daniel","daniel","luis","luis","luis","luis" };
+    vector<string> time = { "10:00","10:40","11:00","09:00","11:00","13:00","15:00" };
+    vector<string> res = alertNames(name, time);
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
