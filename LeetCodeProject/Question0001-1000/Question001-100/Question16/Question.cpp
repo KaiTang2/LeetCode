5c059fe4d2ed33b61e2b6b1bad3331e898abe9c3
KaiTang2 <1347102146@qq.com>
2021-04-22 22:54:34 +0800

    Question 363 16

---
+++ LeetCodeProject/Question0001-1000/Question001-100/Question16/Question.cpp
@@ -0,0 +1,78 @@
+﻿/*
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+ 
+
+示例：
+
+输入：nums = [-1,2,1,-4], target = 1
+输出：2
+解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ 
+
+提示：
+
+3 <= nums.length <= 10^3
+-10^3 <= nums[i] <= 10^3
+-10^4 <= target <= 10^4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum-closest
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+#include"Question.h"
+#include<algorithm>
+int threeSumClosest(vector<int>& nums, int target) {
+    sort(nums.begin(), nums.end());
+    int res = 10000;
+    for (int i = 0; i < nums.size() - 2; ++i)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        int l = i + 1, r = nums.size() - 1;
+        while (l < r)
+        {
+            int sum = nums[i] + nums[l] + nums[r];
+            if (sum == target)
+            {
+                return target;
+            }
+            if (abs(sum - target) < abs(res - target))
+            {
+                res = sum;
+            }
+            if (sum > target)
+            {
+                int r0 = r - 1;
+                while (l < r0 && nums[r0] == nums[r])
+                {
+                    --r0;
+                }
+                r = r0;
+            }
+            else
+            {
+                int l0 = l + 1;
+                while (l0 < r && nums[l] == nums[l0])
+                {
+                    ++l0;
+                }
+                l = l0;
+            }
+        }
+    }
+    return res;
+
+}
+int main()
+{
+    vector<int> nums = { -1,2,1,-4 };
+    int target = -1;
+    cout << threeSumClosest(nums, target) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
---
+++ LeetCodeProject/Question0001-1000/Question301-400/Question363/Question.cpp
@@ -0,0 +1,62 @@
+﻿/*
+给你一个 m x n 的矩阵 matrix 和一个整数 k ，找出并返回矩阵内部矩形区域的不超过 k 的最大数值和。
+
+题目数据保证总会存在一个数值和不超过 k 的矩形区域。
+
+ 
+
+示例 1：
+
+
+输入：matrix = [[1,0,1],[0,-2,3]], k = 2
+输出：2
+解释：蓝色边框圈出来的矩形区域 [[0, 1], [-2, 3]] 的数值和是 2，且 2 是不超过 k 的最大数字（k = 2）。
+示例 2：
+
+输入：matrix = [[2,2,-1]], k = 3
+输出：3
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-100 <= matrix[i][j] <= 100
+-105 <= k <= 105
+ 
+
+
+*/
+#include"Question.h"
+int sum[110][110];
+int maxSumSubmatrix(vector<vector<int>>& mat, int k) {
+    int m = mat.size(), n = mat[0].size();
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + mat[i - 1][j - 1];
+        }
+    }
+    int ans = INT_MIN;
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int l = i; l <= m; l++) {
+                for (int r = j; r <= n; r++) {
+                    int cur = sum[l][r] - sum[i - 1][r] - sum[l][j - 1] + sum[i - 1][j - 1];
+                    if (cur <= k) {
+                        ans = max(ans, cur);
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+int main()
+{
+    vector < vector<int>> nums = { {2,2,-1} };
+    cout << maxSumSubmatrix(nums, 0) << endl;
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
