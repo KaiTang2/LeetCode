5f366d919b6808ffad771ef4c4bbe187a3191ea9
KaiTang2 <1347102146@qq.com>
2021-05-01 21:27:53 +0800

    Question 690

---
+++ LeetCodeProject/Question0001-1000/Question601-700/Question690/Question.cpp
@@ -0,0 +1,37 @@
+﻿/*
+
+
+*/
+#include"Question.h"
+#include<unordered_map>
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+unordered_map<int, Employee*> mp;
+int dfs(int& id)
+{
+    Employee* cur = mp[id];
+    int all = cur->importance;
+    for (auto val : cur->subordinates)
+    {
+        all += dfs(val);
+    }
+    return all;
+}
+int getImportance(vector<Employee*> employees, int id) {
+    for (auto val : employees)
+    {
+        mp[val->id] = val;
+    }
+    return dfs(id);
+}
+int main()
+{
+
+    cout << "Press any key to exit" << endl;
+    int pass = getchar();
+    return 0;
+}
